c5f0c1ba895c20af72dacf923c585132d1eb98d8
Mehul Patel <mehul8@gmail.com>
2016-04-13 15:26:34 -0700

    update express server

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express();
 
 // Store all environment variables
 app.disable('etag');
-app.set('port', process.env.PORT || 3500);
+app.set('port', process.env.PORT || 8080);
 app.set('env', process.env.NODE_ENV || 'production');
 // App middleware
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(app.get('port'), function(){
   console.log('Fam, go to localhost:' + app.get('port'));
